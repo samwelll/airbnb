d950efe8fe7f37c0b65337581828838e76345cd4
SamWelll <gabillardsamuel@gmail.com>
2017-07-23 09:50:01 +0000

    Upload photo de profil

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'devise-i18n'
+gem 'paperclip'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :fullname, presence:true, length: {maximum: 65}
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
